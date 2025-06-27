080c0b055818d174b959af3de64e30171fd9d4ba
Rochi-costa <rbcosta@mail.austral.edu.ar>
2025-06-26 22:37:58 -0300

    Add files via upload

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -5,7 +5,33 @@ def read_file_to_dict(filename):
     :return: dict - diccionario con listas de montos por producto.
     :raises: FileNotFoundError - si el archivo no existe.
     """
-    return {}
+    ventas = {}
+    try:
+        with open(nombre_archivo, "r") as archivo:
+            contenido = archivo.read().strip()
+            pares = contenido.split(";")
+
+            for par in pares:
+                if par:
+                    try:
+                        producto, valor = par.split(":")
+                        valor = float(valor)
+
+                        if producto not in ventas:
+                            ventas[producto] = []
+                        ventas[producto].append(valor)
+                    except ValueError:
+                        print(f"Error en el par: '{par}', se ignora.")
+    except FileNotFoundError:
+        print(f"'{nombre_archivo}' no fue encontrado.")
+        raise  # Importante capturarlo
+    except Exception as e:
+        print(f"Ocurrió un error inesperado al leer el archivo: {e}")
+        return None
+
+
+    return ventas
+    
 
 
 def process_dict(data):
@@ -14,4 +40,18 @@ def process_dict(data):
     :param data: dict - diccionario a procesar.
     :return: None
     """
-    pass
+    for producto, montos in ventas.items():
+        total = sum(montos)
+        promedio = total / len(montos)
+        print(f"{producto}: ventas totales ${total:.2f}, promedio ${promedio:.2f}")
+
+
+# Ejemplo si se ejecuta como principal
+if __name__ == "__main__":
+    archivo = "datos.txt"
+    try:
+        ventas = read_file_to_dict(archivo)
+        if ventas:
+            process_dict(ventas)
+    except FileNotFoundError:
+        pass  #imprimió el mensaje en la función
